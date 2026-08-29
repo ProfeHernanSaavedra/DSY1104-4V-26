2707aa1cecece7c41d426ac4815095590251ee82
profehernanSaavedra <hern.saavedra@profesor.duoc.cl>
2026-08-29 10:55:23 -0400

    javaSript con botones

--- Semana3/js/app.js
+++ Semana3/js/app.js
@@ -1,5 +1,5 @@
 console.log("Hola soy JavaScript");
-let cantidadProductos = 0;
+//let cantidadProductos = 0;
 cantidadProductos = cantidadProductos + 1;
 console.log(cantidadProductos);
 const nombreCafeteria = "Café Duoc";
@@ -19,5 +19,37 @@ botonBienvenida.addEventListener("click",function(){
     //console.log("El usuario presionó el botón");
     mensajeBienvenida.textContent = 
     "¡Bienvenido! Esperemos que disfrutes los productos";
+
+    botonBienvenida.textContent="Bienvenida mostrada";
 });
 
+//promociones
+const botonPromocion = document.getElementById("boton-promocion");
+const promocion = document.getElementById("promocion");
+
+botonPromocion.addEventListener("click",function(){
+    //promocion.classList.remove("d-none");
+    promocion.classList.toggle("d-none");
+
+    if(promocion.classList.contains("d-none")){
+        botonPromocion.textContent="Mostrar promoción";
+    }
+    else{
+        botonPromocion.textContent="Ocultar Promoción";
+    }
+});
+
+//contador de productos
+const botonesAgregar = document.querySelectorAll(".boton-agregar");
+const contador = document.getElementById("contador-productos");
+
+let cantidadProductos = 0;
+
+botonesAgregar.forEach(function(boton){
+    boton.addEventListener("click",function(){
+        cantidadProductos =cantidadProductos+1;
+
+        contador.textContent=cantidadProductos;
+
+    });
+});
