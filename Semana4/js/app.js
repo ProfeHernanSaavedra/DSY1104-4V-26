a2bd75b24f8330d1681dc40e1701356947f6aee6
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2026-09-03 20:56:45 -0400

    creacion de formulario

---
+++ Semana4/js/app.js
@@ -0,0 +1,59 @@
+console.log("Hola soy JavaScript");
+let cantidadProductos = 0;
+cantidadProductos = cantidadProductos + 1;
+console.log(cantidadProductos);
+const nombreCafeteria = "Café Duoc";
+console.log(nombreCafeteria);
+//nombreCafeteria = "JAIMITO";
+
+// ahora veamos ejemplos de modificacion
+const titulo = document.getElementById("titulo-sitio");
+console.log(titulo);
+
+titulo.textContent = "Bienvenido a Café DUOC";
+
+const botonBienvenida = document.getElementById("boton-bienvenida");
+const mensajeBienvenida = document.getElementById("mensaje-bienvenida");
+
+botonBienvenida.addEventListener("click",function(){
+    //console.log("El usuario presionó el botón");
+    mensajeBienvenida.textContent = 
+    "¡Bienvenido! Esperemos que disfrutes los productos";
+
+    botonBienvenida.textContent="Bienvenida mostrada";
+});
+
+//promociones
+const botonPromocion = document.getElementById("boton-promocion");
+const promocion = document.getElementById("promocion");
+
+botonPromocion.addEventListener("click",function(){
+    //promocion.classList.remove("d-none");
+    promocion.classList.toggle("d-none");
+
+    if(promocion.classList.contains("d-none")){
+        botonPromocion.textContent="Mostrar promoción";
+    }
+    else{
+        botonPromocion.textContent="Ocultar Promoción";
+    }
+});
+
+//contador de productos
+const botonesAgregar = document.querySelectorAll(".boton-agregar");
+const contador = document.getElementById("contador-productos");
+const ultimoProducto = document.getElementById("ultimo-producto");
+let cantProductos = 0;
+
+botonesAgregar.forEach(function(boton){
+    boton.addEventListener("click",function(){
+        const nombreProducto =boton.dataset.producto;
+
+        cantProductos =cantProductos+1;
+
+        contador.textContent=cantProductos;
+
+        ultimoProducto.textContent =nombreProducto + " fue agregado correctamente";
+
+    });
+});
